fd752d38c4df7a51bdc4327143b53736af215035
Bipul Kumar <Bipul.Kumar@fivecomms.com>
2018-03-31 17:54:24 +0530

    download insurance

--- tourapp/views.py
+++ tourapp/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from tour_packages.models import Topics
 from gallery.models import GalleryMenu
 from testimonial.models import Testimonial
+from insurance.models import Insurance
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -10,10 +11,12 @@ class HomeView(TemplateView):
         tour_packages = Topics.objects.filter(status = True)
         gallery_menus = GalleryMenu.objects.filter(status = True)
         testimonial_details = Testimonial.objects.all()
-        return render(request, 'home.html', context={ 'tour_packages': tour_packages, 'gallery_menus': gallery_menus, 'testimonial_details': testimonial_details })
+        insurance_details = Insurance.objects.latest('id')
+        return render(request, 'home.html', context={ 'tour_packages': tour_packages, 'gallery_menus': gallery_menus, 'testimonial_details': testimonial_details, 'insurance_details': insurance_details })
 
 class Contact(TemplateView):
     def get(self, request, **kwargs):
         tour_packages = Topics.objects.filter(status = True)
         gallery_menus = GalleryMenu.objects.filter(status = True)
         return render(request, 'contact.html', context={ 'tour_packages': tour_packages, 'gallery_menus': gallery_menus })
+ 
