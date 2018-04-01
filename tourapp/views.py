2e1449f41bf8514229828de0978fcabb36c513b7
Bipul Kumar <Bipul.Kumar@fivecomms.com>
2018-04-01 10:07:22 +0530

    news frontend

--- tourapp/views.py
+++ tourapp/views.py
@@ -4,19 +4,21 @@ from tour_packages.models import Topics
 from gallery.models import GalleryMenu
 from testimonial.models import Testimonial
 from insurance.models import Insurance
+from news.models import NewsInfo
 
 # Create your views here.
 class HomeView(TemplateView):
     def get(self, request, **kwargs):
         tour_packages = Topics.objects.filter(status = True)
         gallery_menus = GalleryMenu.objects.filter(status = True)
+        new_infos = NewsInfo.objects.all()
         testimonial_details = Testimonial.objects.all()
         try:
             insurance_details = Insurance.objects.latest('id')
         except:
             insurance_details = None
             
-        return render(request, 'home.html', context={ 'tour_packages': tour_packages, 'gallery_menus': gallery_menus, 'testimonial_details': testimonial_details, 'insurance_details': insurance_details })
+        return render(request, 'home.html', context={ 'tour_packages': tour_packages, 'gallery_menus': gallery_menus, 'testimonial_details': testimonial_details, 'insurance_details': insurance_details, 'new_infos': new_infos })
 
 class Contact(TemplateView):
     def get(self, request, **kwargs):
