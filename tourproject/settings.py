a8ff4769c994e587344dac4bef03789dfb50d32b
mukulkkumar <mukulkkumarr@gmail.com>
2018-09-02 20:41:15 +0530

    changes in setting file for database

--- tourproject/settings.py
+++ tourproject/settings.py
@@ -94,10 +94,20 @@ WSGI_APPLICATION = 'tourproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tourdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '13.59.6.119',
     }
 }
 
